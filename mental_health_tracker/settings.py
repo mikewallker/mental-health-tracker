c6e1a1af85d8646ee775c753cc99e52c4361bbb7
mikewallker <yosiajosephc@gmail.com>
2024-09-18 16:00:15 +0700

    finishing tutorial 3 + create new pws project

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=96w_c+ds%on=51$dy59y5y-9uj&s6xfhke&6%-_97^#u%y@zq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "yosia-joseph-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
@@ -123,4 +125,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://<URL PWS KAMU>", "https://<URL_PWS_KAMU>"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://yosia-joseph-mentalhealthtracker.pbp.cs.ui.ac.id", "https://yosia-joseph-mentalhealthtracker.pbp.cs.ui.ac.id"]
